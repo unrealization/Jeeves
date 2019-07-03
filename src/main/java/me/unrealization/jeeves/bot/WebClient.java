1079db120834b02f02a5f2ad21c703188ec870b9
Dennis Wronka <dennis@pc.dennis>
2019-07-04 01:18:34 +0200

    Implemented reacting to HTTP 429 (too many requests) response code by waiting the appropriate time. Should work, but somehow doesn't. At least with EDSM...

--- src/main/java/me/unrealization/jeeves/bot/WebClient.java
+++ src/main/java/me/unrealization/jeeves/bot/WebClient.java
@@ -9,7 +9,7 @@ import java.net.URL;
 
 public class WebClient
 {
-	public static String getPage(URL url, String method, String parameters) throws IOException
+	private static String getPage(URL url, String method, String parameters, boolean allowRetry) throws IOException
 	{
 		HttpURLConnection connection = (HttpURLConnection)url.openConnection();
 		connection.setRequestMethod(method);
@@ -26,7 +26,31 @@ public class WebClient
 
 		int responseCode = connection.getResponseCode();
 
-		if (responseCode != 200)
+		if ((responseCode == 429) && (allowRetry == true))
+		{
+			int delay = connection.getHeaderFieldInt("Retry-After", -2) + 1;
+			System.out.println("Retrying after " + Integer.toString(delay) + " seconds.");
+
+			if (delay == -1)
+			{
+				System.out.println("Server wants me to wait, but doesn't tell me how long.");
+				return null;
+			}
+
+			try
+			{
+				Thread.sleep(delay * 1000);
+			}
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+				return null;
+			}
+
+			return WebClient.getPage(url, method, parameters, false);
+		}
+
+		if (responseCode != HttpURLConnection.HTTP_OK)
 		{
 			System.out.println("Status Code: " + responseCode);
 			return null;
@@ -46,20 +70,25 @@ public class WebClient
 		return response.toString();
 	}
 
+	public static String getPage(URL url, String method, String parameters) throws IOException
+	{
+		return WebClient.getPage(url, method, parameters, true);
+	}
+
 	public static String getPage(URL url, String method) throws IOException
 	{
-		return WebClient.getPage(url, method, "");
+		return WebClient.getPage(url, method, "", true);
 	}
 
 	public static String getPage(URL url) throws IOException
 	{
-		return WebClient.getPage(url, "GET", "");
+		return WebClient.getPage(url, "GET", "", true);
 	}
 
 	public static String getPage(String address, String method, String parameters) throws IOException
 	{
 		URL url = new URL(address);
-		return WebClient.getPage(url, method, parameters);
+		return WebClient.getPage(url, method, parameters, true);
 	}
 
 	public static String getPage(String address, String method) throws IOException
